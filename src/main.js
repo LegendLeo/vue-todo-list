1f7d7da03edf28e58b226ee06c9956ed8c3c27d7
huqing1473 <huqing1473@emao.com>
2019-03-05 14:10:47 +0800

    :sparkles: 增加PWA功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { Button, Cell, CellGroup, Checkbox, CheckboxGroup, Field, Icon, Dialog, Notify, Toast } from 'vant'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(Button)
